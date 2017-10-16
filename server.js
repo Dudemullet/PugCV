987b893612c26f6456ba9da7ace6e81d5164b948
Pipe Gutierrez <luis.gutierrez@konfio.mx>
2017-10-15 19:30:35 -0500

    Removes unnecessary config from server

--- server.js
+++ server.js
@@ -7,21 +7,10 @@ var express = require('express'),
   dir  = require('node-dir');
 
 module.exports = function(conf){
-
-  var conf = {
-    dir : path.join(__dirname, 'app'),
-    port : 8080
-  };
-
   app.use(require('connect-livereload')());
   app.use(express.static(conf.dir));
   app.set("views", conf.dir)
   app.engine('jade', require('jade').__express);
-
-  app.get("/", function(req, res, next) {
-      res.render("index.jade");
-  });
-
   app.listen(conf.port);
   console.log("Server started in http://localhost:" + conf.port);
 }
